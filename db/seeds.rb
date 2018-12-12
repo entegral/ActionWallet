18b038e789fd4f1c2c1ffeea9bd61ae3608fd215
Daniel Bennett, Phil Mass, Robert Bruce and Rohan Moore <daniel@>
2018-12-12 10:49:23 -0800

    Add addtional articles and activate bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 article_list = [
-  ["Orignal Bitcoin Whitepaper", "https://bitcoin.org/bitcoin.pdf"]
+  ["Orignal Bitcoin Whitepaper", "https://bitcoin.org/bitcoin.pdf"],
+  ["Tron Black: Ravencoin - Getting Started", "https://medium.com/@tronblack/ravencoin-getting-started-96ab8e3e919b"],
+  ["Tron Black: Ravencoin as an Asset Platform", "https://medium.com/@tronblack/ravencoin-as-an-asset-platform-6979530d125f"],
+  ["Tron Black: Ravencoin Asset FAQ", "https://medium.com/@tronblack/ravencoin-asset-faq-e0d04d460e9b"],
 ]
+
+
+
 article_list.each do |name, url|
   Article.create ({ :name => name, :url => url })
 end
