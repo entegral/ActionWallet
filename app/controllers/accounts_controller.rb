a6d273a3c769f5c8927e381f33d0048cb9e8499f
Daniel Bennett, Phil Mass, Robert Bruce and Rohan Moore <robert@>
2018-12-13 15:58:39 -0800

    update account routes and controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,3 +1,13 @@
 class AccountsController < ApplicationController
+  def index
+    if current_user
+      render :index
+    else
+      redirect_to signin_path
+    end
+  end
 
+  def show
+    @account = Account.find(params[:id])
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if @user
       flash[:notice] = "You've signed in."
       session[:user_id] = @user.id
-      redirect_to "/"
+      redirect_to accounts_path
     else
       flash[:alert] = "There was a problem signing in.  Please try again."
       redirect_to signin_path
