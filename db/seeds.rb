f4c56de7a592fc937e85f5fe51e5bea01cfc8fd3
Daniel Bennett, Phil Mass, Robert Bruce and Rohan Moore <robert@>
2018-12-12 16:19:51 -0800

    add additional articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ article_list = [
   ["Tron Black: Ravencoin - Getting Started", "https://medium.com/@tronblack/ravencoin-getting-started-96ab8e3e919b"],
   ["Tron Black: Ravencoin as an Asset Platform", "https://medium.com/@tronblack/ravencoin-as-an-asset-platform-6979530d125f"],
   ["Tron Black: Ravencoin Asset FAQ", "https://medium.com/@tronblack/ravencoin-asset-faq-e0d04d460e9b"],
+  ["Tron Black: Seeds of Freedom", "https://medium.com/@tronblack/ravencoin-seeds-of-freedom-a3a3ff0fa1"],
+  ["Ravencoin.org", "https://ravencoin.org"]
 ]
 
 Article.destroy_all
