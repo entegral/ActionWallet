7a8ebb9bf3d3f2dcfd829f95aa567fbbd379113f
Rohan Moore <rohanpmoore@gmail.com>
2018-12-17 15:44:39 -0800

    remove messy code fragments

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -2,7 +2,6 @@ require 'faraday'
 require 'json'
 
 class Connection
-  # BASE = 'https://sandbox-api.coinmarketcap.com'
   def self.api(params)
     Faraday.new(url: params.fetch(:base)) do |faraday|
       faraday.response :logger
@@ -12,6 +11,3 @@ class Connection
     end
   end
 end
-
-# 'X-CMC_PRO_API_KEY'
-# 'd5a521bc-0fa5-4f29-8cc8-536713e5de51'
