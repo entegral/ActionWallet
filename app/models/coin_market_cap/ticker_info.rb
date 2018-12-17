e99cf773560c8e254b5887825d024d2e20f738a8
Rohan Moore <rohanpmoore@gmail.com>
2018-12-17 15:25:10 -0800

    add ability to reference multiple APIs

--- app/models/coin_market_cap/ticker_info.rb
+++ app/models/coin_market_cap/ticker_info.rb
@@ -1,7 +1,8 @@
 module CoinMarketCap
   class TickerInfo
     def self.get_currency_details(currency_id)
-      response = Request.where('/v1/cryptocurrency/quotes/latest', {id: currency_id})
+      coin_market_pathway = {base: 'https://sandbox-api.coinmarketcap.com', key_link: 'X-CMC_PRO_API_KEY', key: 'd5a521bc-0fa5-4f29-8cc8-536713e5de51'}
+      response = Request.where('/v1/cryptocurrency/quotes/latest', {id: currency_id}, coin_market_pathway)
       details = response.fetch("data").fetch("#{currency_id}")
       name = details.fetch("name")
       price = details.fetch("quote").fetch("USD").fetch("price")
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -2,13 +2,16 @@ require 'faraday'
 require 'json'
 
 class Connection
-  BASE = 'https://sandbox-api.coinmarketcap.com'
-  def self.api
-    Faraday.new(url: BASE) do |faraday|
+  # BASE = 'https://sandbox-api.coinmarketcap.com'
+  def self.api(params)
+    Faraday.new(url: params.fetch(:base)) do |faraday|
       faraday.response :logger
       faraday.adapter Faraday.default_adapter
       faraday.headers['Content-Type'] = 'application/json'
-      faraday.headers['X-CMC_PRO_API_KEY'] = 'd5a521bc-0fa5-4f29-8cc8-536713e5de51'
+      faraday.headers["#{params.fetch(:key_link)}"] = params.fetch(:key)
     end
   end
 end
+
+# 'X-CMC_PRO_API_KEY'
+# 'd5a521bc-0fa5-4f29-8cc8-536713e5de51'
