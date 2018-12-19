6ba7b63ddf154d209a2313e24525dcf2156fb1ea
Daniel Bennett, Phil Mass, Robert Bruce and Rohan Moore <phil@>
2018-12-19 10:43:55 -0800

    add blockcypher api for calculating account totals

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'faraday'
 gem 'money-tree'
 # BIP0039
 gem 'bip_mnemonic', '~> 0.0.2'
+gem 'blockcypher-ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,19 @@
+require 'blockcypher'
+
+
+
 class Account < ActiveRecord::Base
   belongs_to :user
   has_many :addresses
   validates :user_id, :name, :coin, :account_index, :presence => true
+
+  def total
+    block_cypher = BlockCypher::Api.new(currency:BlockCypher::BTC, network:BlockCypher::TEST_NET_3, api_token:"772f7e3b20e74c4881c1e0d5c8e7e1c4")
+
+    total = 0.00000000
+    self.addresses.each do |address|
+      total += block_cypher.address_final_balance(address.public_address(self.user.master_key, true))
+    end
+    total/100000000.00000000
+  end
 end
